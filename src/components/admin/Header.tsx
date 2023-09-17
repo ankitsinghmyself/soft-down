90bd7f80a6064c37d6560e068954a7c20a1ad995
Ankit Singh <ankitsinghmyself@gmail.com>
2023-09-17 11:36:13 +0530

    icon fix

--- src/components/admin/Header.tsx
+++ src/components/admin/Header.tsx
@@ -17,6 +17,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { useRouter, usePathname } from "next/navigation";
 import { ChevronRightIcon } from "@chakra-ui/icons";
+import { MdLogout, MdPortrait, MdUsbOff } from "react-icons/md";
 
 const Header: React.FC = () => {
   const pathname = usePathname();
@@ -51,55 +52,48 @@ const Header: React.FC = () => {
   };
   return (
     <>
-  <Flex
-    rounded="md"
-    justify="space-between"
-    className="bg-blue-400 text-white items-center p-4"
-  >
-    <Flex>
-      <a href="/">
-        Home <ChevronRightIcon color="gray.500" />
-      </a>
-      {pathname && (
-        <>
-          <Breadcrumb
-            spacing="8px"
-            separator={<ChevronRightIcon color="gray.500" />}
-          >
-            <BreadcrumbItem>
-              <BreadcrumbLink href={pathname} className="">
-                {toTitleCase(pathname.split("/").pop())}
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-          </Breadcrumb>
-        </>
-      )}
-    </Flex>
-
-    <Menu>
-      <MenuButton className="flex">
-        {userData && (
-          <Flex alignItems="center">
-            <Avatar
-              size="xs"
-              name={userData.userName}
-              src={userData.userAvatarUrl}
-              marginRight="2"
-              borderRadius="full"
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
-              cursor="pointer"
-              className="w-[32px]"
-            />
-            <p className="mt-1">{userData.userName.toUpperCase()}</p>
-          </Flex>
+   <Flex rounded="md" justify="space-between" bg="blue.400" color="white" p={4}>
+      <Flex>
+        <a href="/">
+          Home <ChevronRightIcon color="gray.500" />
+        </a>
+        {pathname && (
+          <>
+            <Breadcrumb spacing="8px" separator={<ChevronRightIcon color="gray.500" />}>
+              <BreadcrumbItem>
+                <BreadcrumbLink href={pathname} className="">
+                  {toTitleCase(pathname.split("/").pop())}
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+            </Breadcrumb>
+          </>
         )}
-      </MenuButton>
-      <MenuList bg={"lightgray"} p={"10"} borderRadius={"5px"}>
-        <MenuItem onClick={() => setIsMenuOpen(false)}>Profile</MenuItem>
-        <MenuItem onClick={handleLogout}>Logout</MenuItem>
-      </MenuList>
-    </Menu>
-  </Flex>
+      </Flex>
+
+      <Menu>
+        <MenuButton className="flex"  >
+          {userData && (
+            <Flex alignItems="center">
+              <Avatar
+                size="xs"
+                name={userData.userName}
+                src={userData.userAvatarUrl}
+                marginRight="2"
+                borderRadius="full"
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                cursor="pointer"
+                className="w-[32px]"
+              />
+              <p className="mt-1">{userData.userName.toUpperCase()}</p>
+            </Flex>
+          )}
+        </MenuButton>
+        <MenuList bg="blue.300" p="5" borderRadius="5px" >
+          <MenuItem  bg="blue.300" onClick={() => setIsMenuOpen(false)}>Profile <MdPortrait /> </MenuItem>
+          <MenuItem  bg="blue.300" onClick={handleLogout}>Logout <MdLogout/> </MenuItem>
+        </MenuList>
+      </Menu>
+    </Flex>
 </>
 
   );
